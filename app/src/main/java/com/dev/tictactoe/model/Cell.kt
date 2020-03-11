d7addbaf7ede815b7f09bef466c4935651d7ad99
2020-DEV-058 <>
2020-03-11 16:49:38 +0530

    Refactored implementation of cells comparison and moved it to explicit.

--- app/src/main/java/com/dev/tictactoe/model/Cell.kt
+++ app/src/main/java/com/dev/tictactoe/model/Cell.kt
@@ -1,15 +1,21 @@
 package com.dev.tictactoe.model
 
 class Cell(var player: Player?){
+    companion object {
+        const val INDEX_ZERO = 0
+        const val INDEX_ONE = 1
+    }
     val isEmptyCell = player == null || player?.value.isNullOrEmpty()
 
     fun hasCellValuesAreEqual(vararg cells: Cell): Boolean {
-        if (cells.isEmpty()) return false
-        for (cell in cells)
-            if (cell.player?.value.isNullOrEmpty()) return false
-        val comparisonBase = cells[0]
-        for (i in 1 until cells.size)
-            if (!comparisonBase.player?.value.equals(cells[i].player?.value)) return false
-        return true
+        if (isEmptyCell(cells)) return false
+        cells.forEach { cell -> when { isPlayerValueIsEmpty(cell) -> return false } }
+        val comparisonBase = cells[INDEX_ZERO]
+        return (INDEX_ONE until cells.size).all { isPlayerValuesAreSame(comparisonBase, cells, it) }
     }
+
+    private fun isPlayerValuesAreSame(comparisonBase: Cell, cells: Array<out Cell>, index: Int) = getPlayerValue(comparisonBase).equals(getPlayerValue(cells[index]))
+    private fun isPlayerValueIsEmpty(cell: Cell) = getPlayerValue(cell).isNullOrEmpty()
+    private fun isEmptyCell(cells: Array<out Cell>) = cells.isEmpty()
+    private fun getPlayerValue(comparisonBase: Cell) = comparisonBase.player?.value
 }
