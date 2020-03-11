d51093d1c143ca7416b29299fb8df49200fb15b9
2020-DEV-058 <>
2020-03-11 17:03:22 +0530

    Refactored implementation of winner in Board and moved it to explicit.

--- app/src/main/java/com/dev/tictactoe/model/Board.kt
+++ app/src/main/java/com/dev/tictactoe/model/Board.kt
@@ -2,7 +2,7 @@ package com.dev.tictactoe.model
 
 class Board(var playerOne: String, var playerTwo: String) {
 
-    val player1 =  Player(playerOne,  PLAYER_ONE_VALUE)
+    val player1 =  Player(playerOne, PLAYER_ONE_VALUE)
     val player2 = Player(playerTwo, PLAYER_TWO_VALUE)
     var currentPlayer = player1
     var cells = Array(BOARD_SIZE) { Array(BOARD_SIZE) { Cell(null) } }
@@ -12,20 +12,27 @@ class Board(var playerOne: String, var playerTwo: String) {
         const val PLAYER_ONE_VALUE = "X"
         const val PLAYER_TWO_VALUE = "O"
         const val BOARD_SIZE = 3
+        const val INDEX_ZERO = 0
     }
 
-    fun isWinnerAvailable(): Boolean {
-        for (index in 0 until BOARD_SIZE)
-            if (cell.getHorizontalCells(cells, index))
-                return true
-        for (index in 0 until BOARD_SIZE)
-            if (cell.getVerticalCells(cells, index))
-                return true
-        for (i in 0 until BOARD_SIZE)
-            if (cell.getDiagonalCellsFromLeftToRight(cells) || cell.getDiagonalFromRightToLeft(cells))
-                return true
+    fun isWinnerAvailable(): Boolean = hasThreeSameHorizontalCells() || hasThreeSameVerticalCells() || hasThreeSameDiagonalCells()
+
+    private fun hasThreeSameDiagonalCells(): Boolean = when { compareValuesFromLeft() || compareValuesFromRight() -> true else -> false }
+
+    private fun hasThreeSameVerticalCells(): Boolean {
+        (INDEX_ZERO until BOARD_SIZE).forEach { index -> when { compareValuesByColumn(index) -> return true } }
         return false
     }
+
+    private fun hasThreeSameHorizontalCells(): Boolean {
+        (INDEX_ZERO until BOARD_SIZE).forEach { index -> when { compareValuesByRow(index) -> return true } }
+        return false
+    }
+
+    private fun compareValuesFromRight() = cell.getDiagonalFromRightToLeft(cells)
+    private fun compareValuesFromLeft() = cell.getDiagonalCellsFromLeftToRight(cells)
+    private fun compareValuesByRow(index: Int) = cell.getHorizontalCells(cells, index)
+    private fun compareValuesByColumn(index: Int) = cell.getVerticalCells(cells, index)
 
     fun switchPlayer() = when (currentPlayer) { player1 -> currentPlayer = player2 else -> currentPlayer = player1 }
 }
